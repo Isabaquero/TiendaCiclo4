51b9af8a76950fe583898e1f87ab8cc67dc00f18
diegobeltranc4 <43439879+diegobeltranc4@users.noreply.github.com>
2021-12-04 09:45:04 -0500

    Reporte de clientes
    Reporte de clientes funcional

--- src/router/reportesclientes.js
+++ src/router/reportesclientes.js
@@ -1,12 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    //falta validar
-    //res.render('ventas', {session: req.session.info});
-    req.session.info =  {name:'admininicial', password:'admin123456', lang:'es', 'role':0, '_id':01, ubication:'bogota'};
-    if( req.session.info ) res.render('reportesclientes', {session: req.session.info});
-    else res.redirect('/');
+const client = require("../model/client");
+router.get("/", async (req, res) => {
+  try {
+    const arrayClientBD = await client.find();
+    console.log(arrayClientBD);
+    req.session.info = {
+      name: "admininicial",
+      password: "admin123456",
+      lang: "es",
+      role: 0,
+      _id: 01,
+      ubication: "bogota",
+    };
+    if (req.session.info) {
+      //falta validar
+      //res.render('ventas', {session: req.session.info});
+      res.render("reportesclientes", {
+        session: req.session.info,
+        arrayClients: arrayClientBD
+      });
+    } else res.redirect("/");
+  } catch (error) {}
 });
 
 module.exports = router;
